f6b540df5405f02c58fa9b2f5d9afb4a4053d7c1
AlunoTI <AlunoTI@estacio.acad>
2024-09-09 20:35:12 -0300

    test andriod studio

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View ,Button, Image} from 'react-native';
+import icon from './assets/icon.png'
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      {/* <Image source={icon} ></Image> */}
+      <Text
+      selectable={true}
+      selectionColor="red">Open up App.js as as to start working on your app!</Text>
+      <Text>Maduro é gente boa :|</Text>
+      <Text>Maduro :|</Text>
+      <StatusBar style="auto" />
+
+      <Button
+      title= "sldkjfhjdsk"
+      color="red">
+
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
