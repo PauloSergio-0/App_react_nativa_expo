e80feef06b6b72e801c38712ac3e07bf842d4945
AlunoTI <AlunoTI@estacio.acad>
2024-09-16 20:37:22 -0300

    test estilo

---
+++ styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native';
+
+
+export const Conteiner = styled.View`
+    background-color: red;
+    flex:1;
+    aling-items: center;
+    justify-content: center;
+`;
